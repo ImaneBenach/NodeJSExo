217c424760dde27c8a93713d8bc373bcc6352f46
Imane <imane@MacBook-Pro-de-Imane.local>
2020-03-16 15:48:40 +0100

    server

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+app.get('/hello', function(req, res){
+  if(req.query.nom == null){
+    res.send('Quel est votre nom ?')
+  }
+  res.send('Bonjour, ' + req.query.nom)
+})
+
 app.listen(3000, function () {
   console.log('Listening on port 3000!')
 })
