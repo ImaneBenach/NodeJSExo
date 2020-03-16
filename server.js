192ce3c72e1ab2ddf5c2f576e96a35496f8db1b9
Imane <imane@MacBook-Pro-de-Imane.local>
2020-03-16 17:15:09 +0100

    server

--- server.js
+++ server.js
@@ -2,22 +2,42 @@
 const express = require('express')
 const app = express()
 
+// exo 1
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
+// exo 2 : deploiement en production
 
+// exo 3
 app.get('/hello', function(req, res){
-  if(req.query.nom == null){
-    res.send('Quel est votre nom ?')
+  if(req.query.nom == "" || req.query.nom == undefined){
+    res.send("Quel est votre nom ?")
   }
-  res.send('Bonjour, ' + req.query.nom+ " !")
+  res.send("Bonjour, " + req.query.nom+ " !")
 })
 
-app.post('/message', function(req,res,next) {
-  console.dir(req.body, {depth: 1})
-  res.sendStatus(200);
-})
+// exo 4
+app.post('/chat', function(req, res){
+
+  if(msg != "" ){
+
+      switch(msg){
+
+          case "ville":
+              res.send("Nous sommes à Paris")
+              break
+
+          case "météo":
+              res.send("Il fait beau")
+              break
+      }
+  }
+
+});
+
+//exo 5
+////
 
 app.listen(3000, function () {
-  console.log('Listening on port 3000!')
+  console.log('Listening on port 3000! :)')
 })
