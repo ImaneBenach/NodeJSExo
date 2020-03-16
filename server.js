9329acd349cd1456b16dd150dcb277e8fc8c4067
Imane <imane@MacBook-Pro-de-Imane.local>
2020-03-16 15:10:18 +0100

    server

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello Imane!')
+  res.send('Hello World!')
 })
 
 app.listen(4000, function () {
-  console.log('Listening on port 3000!')
+  console.log('Listening on port 4000!')
 })
