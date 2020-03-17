0f8870972c9139d2c30535d98a0593c10f101b9d
Imane <imane@MacBook-Pro-de-Imane.local>
2020-03-17 10:52:37 +0100

    server

--- server.js
+++ server.js
@@ -1,43 +1,60 @@
 
 const express = require('express')
 const app = express()
+app.use(express.json())
+
 const PORT = process.env.PORT || 3000 ; 
 
-// exo 1
+//Exercice 1
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
-// exo 2 : deploiement en production
 
-// exo 3
+// Exercice 3
+app.get('/hello', function(req, res){
+  const nom = req.query.nom
+  if(nom) {
+    res.send("Bonjour, " + nom + " !")
+  } else {
+    res.send("Quel est votre nom ?")
+  }
+})
+/*
 app.get('/hello', function(req, res){
   if(req.query.nom == "" || req.query.nom == undefined){
     res.send("Quel est votre nom ?")
   }
   res.send("Bonjour, " + req.query.nom+ " !")
 })
+*/
 
-// exo 4
+// Exercice 4 
+app.post('/chat', function(req,res) {
+  if (req.body.msg == "ville") {
+    res.send("Nous sommes à Paris")
+  } else if(req.body.msg == "météo"){
+    res.send("Il fait beau")
+  } else {
+    res.send(req.body.msg)
+  }
+})
+/*
 app.post('/chat', function(req, res){
-
   if(msg != "" ){
-
       switch(msg){
-
           case "ville":
               res.send("Nous sommes à Paris")
               break
-
           case "météo":
               res.send("Il fait beau")
               break
       }
   }
-
 });
+*/
 
 //exo 5
-////
+
 
 app.listen(3000, function () {
   console.log('Listening on port 3000! :)')
