8a4093ffe2966b71362326d94ec8224a0c235d1f
Imane <imane@MacBook-Pro-de-Imane.local>
2020-03-17 14:06:28 +0100

    server

--- server.js
+++ server.js
@@ -1,33 +1,77 @@
+
 const express = require('express')
 const app = express()
+app.use(express.json())
+const fs = require('fs')
 
-app.use(express.json()) // for parsing application/json
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000 ; 
 
+//Exercice 1
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.get('/hello', function (req, res) {
+// Exercice 3
+app.get('/hello', function(req, res){
   const nom = req.query.nom
-  if (nom) {
-    res.send('Bonjour, ' + nom + ' !')
+  if(nom) {
+    res.send("Bonjour, " + nom + " !")
   } else {
-    res.send('Quel est votre nom ?')
+    res.send("Quel est votre nom ?")
   }
 })
+/*
+app.get('/hello', function(req, res){
+  if(req.query.nom == "" || req.query.nom == undefined){
+    res.send("Quel est votre nom ?")
+  }
+  res.send("Bonjour, " + req.query.nom+ " !")
+})
+*/
 
+// Exercice 4 + 5
 app.post('/chat', function (req, res) {
   if (req.body.msg === 'ville') {
     res.send('Nous sommes à Paris')
   } else if (req.body.msg === 'météo') {
     res.send('Il fait beau')
   } else {
-    res.send(req.body.msg)
+    if (/ = /.test(req.body.msg)) {
+      // const [ cle, valeur ] = req.body.msg.split(' = ')
+      const parties = req.body.msg.split(' = ')
+      const cle = parties[0]
+      const valeur = parties[1]
+      // const objet = { [cle]: valeur }
+      const objet = {}
+      objet[cle] = valeur
+      fs.writeFileSync('réponses.json', JSON.stringify(objet))
+    } else {
+      const cle = req.body.msg
+      const reponses = fs.readFileSync('réponses.json', { encoding: 'utf8' })
+      const reponse = JSON.parse(reponses)[cle]
+      console.log('réponses:', JSON.parse(reponses)[cle])
+      res.send(reponse)
+    }
   }
 })
 
-app.listen(PORT, function () {
-  console.log('Example app listening on port ' + PORT)
+/*
+app.post('/chat', function(req, res){
+  if(msg != "" ){
+      switch(msg){
+          case "ville":
+              res.send("Nous sommes à Paris")
+              break
+          case "météo":
+              res.send("Il fait beau")
+              break
+      }
+  }
+});
+*/
+
+
+app.listen(3000, function () {
+  console.log('Listening on port 3000! :)')
 })
