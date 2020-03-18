0aacf7c6abeb799daa0be9864895b8918c5484d7
Imane <imane@MacBook-Pro-de-Imane.local>
2020-03-18 13:51:31 +0100

    server

--- server.js
+++ server.js
@@ -3,19 +3,22 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 const fs = require('fs')
+const assert = require('assert');
 const MongoClient = require('mongodb').MongoClient;
-
+const url = 'mongodb://localhost:27017/chat-bot';
+const dbName = 'chat-bot';
 const PORT = process.env.PORT || 3000 ; 
 
-// MongoDB
-const uri = "mongodb+srv://Test:Sohan12@cluster-huy2l.azure.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useUnifiedTopology: true } ,{ useNewUrlParser: true });
-console.log("connected")
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
+(async function() {
+  const client = new MongoClient(url, { useUnifiedTopology: true } );
+  try {
+    await client.connect();
+    console.log("connected correctly");
+  } catch (err) {
+    console.log(err.stack);
+  }
   client.close();
-});
+})();
 
 //Exercice 1
 app.get('/', function (req, res) {
@@ -31,17 +34,9 @@ app.get('/hello', function(req, res){
     res.send("Quel est votre nom ?")
   }
 })
-/*
-app.get('/hello', function(req, res){
-  if(req.query.nom == "" || req.query.nom == undefined){
-    res.send("Quel est votre nom ?")
-  }
-  res.send("Bonjour, " + req.query.nom+ " !")
-})
-*/
 
 // Exercice 4 + 5
-app.post('/chat', function (req, res) {
+app.post('/chat', async function (req, res) {
   if (req.body.msg === 'ville') {
     res.send('Nous sommes à Paris')
   } else if (req.body.msg === 'météo') {
@@ -49,43 +44,51 @@ app.post('/chat', function (req, res) {
   } else {
     if (/ = /.test(req.body.msg)) {
       const [ cle, valeur ] = req.body.msg.split(' = ')
-      readValuesFromFile()
-        .catch(err => {
-          res.send('error while reading réponses.json', err)
-        })
-        .then(valeursExistantes => {
-          const data = JSON.stringify({
-            ...valeursExistantes,
-            [cle]: valeur
-          })
-          return writeFile('réponses.json', data)
-        })
-        .then(() => {
-          res.send('Merci pour cette information !')
-        })
-        .catch((err) => {
-          console.error('error while saving réponses.json', err)
-          res.send('Il y a eu une erreur lors de l\'enregistrement')
-        })
+      let valeursExistantes
+      try {
+        valeursExistantes = await readValuesFromFile();
+      } catch (err) {
+        res.send('error while reading réponses.json', err)
+        return
+      }
+      const data = JSON.stringify({
+        ...valeursExistantes,
+        [cle]: valeur
+      })
+      try {
+        await writeFile('réponses.json', data)
+        res.send('Merci pour cette information !')
+      } catch (err) {
+        console.error('error while saving réponses.json', err)
+        res.send('Il y a eu une erreur lors de l\'enregistrement')
+      }
     } else {
       const cle = req.body.msg
-      readValuesFromFile()
-        .then((values) => {
-          const reponse = values[cle]
-          res.send(cle + ': ' + reponse)
-        })
-        .catch((err) => {
-          res.send('error while reading réponses.json', err)
-        })
+      try {
+        const values = await readValuesFromFile()
+        const reponse = values[cle]
+        res.send(cle + ': ' + reponse)
+      } catch (err) {
+        res.send('error while reading réponses.json', err)
+      }
     }
   }
+})
+
+// PARTIE 2 : Exercice 2
+app.get('/messages/all', function (req, res) {
+  const db = client.db(dbName)
+  const col = db.collection('messages')
+  col.find().toArray(function(err, docs) {
+      return res.json(docs);
+  });
 })
 
 app.listen(3000, function () {
   console.log('Listening on port 3000! :)')
 })
 
-function readValuesFromFile() {
-  return readFile('réponses.json', { encoding: 'utf8' })
-    .then(reponses => JSON.parse(reponses))
+async function readValuesFromFile() {
+  const reponses = await readFile('réponses.json', { encoding: 'utf8' })
+  return JSON.parse(reponses)
 }
